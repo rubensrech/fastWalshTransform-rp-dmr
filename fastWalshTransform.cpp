095982304715fc0e915cd7def565f4c300432999
Rubens Rech <rubensch@icloud.com>
2020-10-17 23:20:41 +0200

    FP64 x FP32 relative and absolute error

--- fastWalshTransform.cpp
+++ fastWalshTransform.cpp
@@ -106,7 +106,11 @@ void dyadicConvolutionCPU(double *h_Result, double *h_Data, double *h_Kernel, in
 // GPU FWT
 ////////////////////////////////////////////////////////////////////////////////
 extern void fwtBatchGPU(double *d_Data, int M, int log2N);
+extern void fwtBatchGPU(float *d_Data, int M, int log2N);
 extern void modulateGPU(double *d_A, double *d_B, int N);
+extern void modulateGPU(float *d_A, float *d_B, int N);
+
+extern void relative_error_gpu(double *output, float *output_rp, float *err_output, int N);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Data configuration
@@ -118,23 +122,25 @@ const int log2Kernel = 7;
 #else
     const   int log2Data = 16;
 #endif
-const int   dataN = 1 << log2Data;
+const int dataN = 1 << log2Data;
 const int kernelN = 1 << log2Kernel;
 
-const int   DATA_SIZE = dataN   * sizeof(double);
+const int DATA_SIZE = dataN   * sizeof(double);
+const int DATA_SIZE_RP = dataN   * sizeof(float);
 const int KERNEL_SIZE = kernelN * sizeof(double);
+const int KERNEL_SIZE_RP = kernelN * sizeof(float);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Main program
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[]) {
-    double *h_Data,
-          *h_Kernel,
-          *h_ResultCPU,
-          *h_ResultGPU;
+    double *h_Data, *h_Kernel, *h_ResultCPU, *h_ResultGPU;
+    float *h_Data_rp, *h_Kernel_rp, *h_ResultGPU_rp;
+    float *h_Error;
 
-    double *d_Data, 
-          *d_Kernel;
+    double *d_Data, *d_Kernel;
+    float *d_Data_rp, *d_Kernel_rp;
+    float *d_Error;
 
     double delta, ref, sum_delta2, sum_ref2, L2norm;
     int i;
@@ -146,33 +152,62 @@ int main(int argc, char *argv[]) {
     h_ResultCPU = (double*)malloc(DATA_SIZE);
     h_ResultGPU = (double*)malloc(DATA_SIZE);
 
+    h_Kernel_rp    = (float*)malloc(KERNEL_SIZE_RP);
+    h_Data_rp      = (float*)malloc(DATA_SIZE_RP);
+    h_ResultGPU_rp = (float*)malloc(DATA_SIZE_RP);
+
+    h_Error = (float*)malloc(DATA_SIZE_RP);
+
     printf("    1.2) Allocating GPU memory\n");
     cudaMalloc((void**)&d_Kernel, DATA_SIZE);
     cudaMalloc((void**)&d_Data,   DATA_SIZE);
 
+    cudaMalloc((void**)&d_Kernel_rp, DATA_SIZE_RP);
+    cudaMalloc((void**)&d_Data_rp,   DATA_SIZE_RP);
+
+    cudaMalloc((void**)&d_Error,   DATA_SIZE_RP);
+
     printf("    1.3) Generating data\n");
     srand((int)time(NULL));
-    for (i = 0; i < kernelN; i++)
+    for (i = 0; i < kernelN; i++) {
         h_Kernel[i] = (double)rand() / (double)RAND_MAX;
+        h_Kernel_rp[i] = h_Kernel[i];
+    }
 
-    for (i = 0; i < dataN; i++)
+    for (i = 0; i < dataN; i++) {
         h_Data[i] = (double)rand() / (double)RAND_MAX;
+        h_Data_rp[i] = h_Data[i];
+    }
 
     cudaMemset(d_Kernel, 0, DATA_SIZE);
     cudaMemcpy(d_Kernel, h_Kernel, KERNEL_SIZE, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_Data,   h_Data,     DATA_SIZE, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_Data, h_Data, DATA_SIZE, cudaMemcpyHostToDevice);
+
+    cudaMemset(d_Kernel_rp, 0, DATA_SIZE_RP);
+    cudaMemcpy(d_Kernel_rp, h_Kernel_rp, KERNEL_SIZE_RP, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_Data_rp, h_Data_rp, DATA_SIZE_RP, cudaMemcpyHostToDevice);
 
 
     printf("2) Running GPU dyadic convolution using Fast Walsh Transform\n");
     cudaThreadSynchronize();
+    // Running FP64
     fwtBatchGPU(d_Data, 1, log2Data);
     fwtBatchGPU(d_Kernel, 1, log2Data);
     modulateGPU(d_Data, d_Kernel, dataN);
     fwtBatchGPU(d_Data, 1, log2Data);
     cudaThreadSynchronize();
 
+    // Running FP32
+    fwtBatchGPU(d_Data_rp, 1, log2Data);
+    fwtBatchGPU(d_Kernel_rp, 1, log2Data);
+    modulateGPU(d_Data_rp, d_Kernel_rp, dataN);
+    fwtBatchGPU(d_Data_rp, 1, log2Data);
+    cudaThreadSynchronize();
+
     printf("    2.1) Reading back GPU results\n");
     cudaMemcpy(h_ResultGPU, d_Data, DATA_SIZE, cudaMemcpyDeviceToHost);
+
+    cudaMemcpy(h_ResultGPU_rp, d_Data_rp, DATA_SIZE_RP, cudaMemcpyDeviceToHost);
 
 
     printf("3) Running straightforward CPU dyadic convolution\n");
@@ -192,11 +227,31 @@ int main(int argc, char *argv[]) {
     printf("    L2 norm: %E\n", L2norm);
     printf((L2norm < 1e-6) ? "    TEST PASSED\n" : "    TEST FAILED\n");
 
-    printf("5) Shutting down\n");
+
+    printf("5) Comparing Double VS Float\n");
+    // Relative error
+    relative_error_gpu(d_Data, d_Data_rp, d_Error, dataN);
+    cudaMemcpy(h_Error, d_Error, DATA_SIZE_RP, cudaMemcpyDeviceToHost);    
+    int iMaxRelErr = 0;
+    for (i = 0; i < dataN; i++) if (h_Error[i] > h_Error[iMaxRelErr]) iMaxRelErr = i;
+    printf("    Max relative error: %f (%f x %f)\n", h_Error[iMaxRelErr], h_ResultCPU[iMaxRelErr], h_ResultGPU_rp[iMaxRelErr]);
+    // Absolute error
+    for (i = 0; i < dataN; i++) h_Error[i] = abs(h_ResultGPU[i] - h_ResultGPU_rp[i]);
+    int iMaxAbsErr = 0;
+    for (i = 0; i < dataN; i++) if (h_Error[i] > h_Error[iMaxAbsErr]) iMaxAbsErr = i;
+    printf("    Max absolute error: %f (%f x %f)\n", h_Error[iMaxAbsErr], h_ResultCPU[iMaxAbsErr], h_ResultGPU_rp[iMaxAbsErr]);
+
+    printf("6) Shutting down\n");
     cudaFree(d_Data);
     cudaFree(d_Kernel);
     free(h_ResultGPU);
     free(h_ResultCPU);
     free(h_Data);
     free(h_Kernel);
+
+    cudaFree(d_Data_rp);
+    cudaFree(d_Kernel_rp);
+    free(h_ResultGPU_rp);
+    free(h_Data_rp);
+    free(h_Kernel_rp);
 }
--- fastWalshTransform_kernel.cu
+++ fastWalshTransform_kernel.cu
@@ -55,7 +55,7 @@ inline void checkCudaError(cudaError_t code, const char *file, int line, bool ab
 ///////////////////////////////////////////////////////////////////////////////
 #define ELEMENTARY_LOG2SIZE 11
 
-__global__ void fwtBatch1Kernel(double *d_Output, float *d_Output_rp, double *d_Input, int log2N) {
+__global__ void fwtBatch1Kernel(double *d_Output, double *d_Input, int log2N) {
     const int N = 1 << log2N;
     int stride = N;
     const int base = blockIdx.x << log2N;
@@ -112,6 +112,63 @@ __global__ void fwtBatch1Kernel(double *d_Output, float *d_Output_rp, double *d_
         d_Dst[pos] = s_data[pos];
 }
 
+__global__ void fwtBatch1Kernel(float *d_Output, float *d_Input, int log2N) {
+    const int N = 1 << log2N;
+    int stride = N;
+    const int base = blockIdx.x << log2N;
+
+    // (2 ** 11) * 4 bytes == 8KB -- maximum s_data[] size for G80
+    extern __shared__ float s_data_rp[];
+    float *d_Src = d_Input  + base;
+    float *d_Dst = d_Output + base;
+
+    for(int pos = threadIdx.x; pos < N; pos += blockDim.x)
+        s_data_rp[pos] = d_Src[pos];
+
+    //Do single radix-2 stage if for odd power
+    if(log2N & 1){
+        __syncthreads();
+        stride >>= 1;
+        for(int pos = threadIdx.x; pos < N / 2; pos += blockDim.x){
+            int lo = pos & (stride - 1);
+            int i0 = ((pos - lo) << 1) + lo;
+            int i1 = i0 + stride;
+
+            float t0 = s_data_rp[i0];
+            float t1 = s_data_rp[i1];
+            s_data_rp[i0] = t0 + t1;
+            s_data_rp[i1] = t0 - t1;
+        }
+    }
+
+    //Main radix4 stages
+    stride >>= 2;
+    int pos = threadIdx.x;
+    for(; stride >= 1; stride >>= 2){
+        __syncthreads();
+        int lo = pos & (stride - 1);
+        int i0 = ((pos - lo) << 2) + lo;
+        int i1 = i0 + stride;
+        int i2 = i1 + stride;
+        int i3 = i2 + stride;
+
+        float d0 = s_data_rp[i0];
+        float d1 = s_data_rp[i1];
+        float d2 = s_data_rp[i2];
+        float d3 = s_data_rp[i3];
+
+        float t;
+        t = d0; d0         = d0 + d2; d2         = t - d2;
+        t = d1; d1         = d1 + d3; d3         = t - d3;
+        t = d0; s_data_rp[i0] = d0 + d1; s_data_rp[i1] = t - d1;
+        t = d2; s_data_rp[i2] = d2 + d3; s_data_rp[i3] = t - d3;
+    }
+
+    __syncthreads();
+    for(int pos = threadIdx.x; pos < N; pos += blockDim.x)
+        d_Dst[pos] = s_data_rp[pos];
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Single in-global memory radix-4 Fast Walsh Transform pass
 // (for strides exceeding elementary vector size)
@@ -141,6 +198,32 @@ __global__ void fwtBatch2Kernel(double *d_Output, double *d_Input, int stride) {
     t = d2; d_Dst[i2] = d2 + d3; d_Dst[i3] = t - d3;
 }
 
+__global__ void fwtBatch2Kernel(float *d_Output, float *d_Input, int stride) {
+    const int pos = blockIdx.x * blockDim.x + threadIdx.x;
+    const int   N = blockDim.x *  gridDim.x * 4;
+
+    float *d_Src = d_Input  + blockIdx.y * N;
+    float *d_Dst = d_Output + blockIdx.y * N;
+
+    int lo = pos & (stride - 1);
+    int i0 = ((pos - lo) << 2) + lo;
+    int i1 = i0 + stride;
+    int i2 = i1 + stride;
+    int i3 = i2 + stride;
+
+    float d0 = d_Src[i0];
+    float d1 = d_Src[i1];
+    float d2 = d_Src[i2];
+    float d3 = d_Src[i3];
+
+    float t;
+    t = d0; d0        = d0 + d2; d2        = t - d2;
+    t = d1; d1        = d1 + d3; d3        = t - d3;
+    t = d0; d_Dst[i0] = d0 + d1; d_Dst[i1] = t - d1;
+    t = d2; d_Dst[i2] = d2 + d3; d_Dst[i3] = t - d3;
+}
+
+
 ////////////////////////////////////////////////////////////////////////////////
 // Put everything together: batched Fast Walsh Transform CPU front-end
 ////////////////////////////////////////////////////////////////////////////////
@@ -156,6 +239,18 @@ void fwtBatchGPU(double *d_Data, int M, int log2N) {
     CHECK_CUDA_ERROR(cudaPeekAtLastError());
 }
 
+void fwtBatchGPU(float *d_Data, int M, int log2N) {
+    int N = 1 << log2N;
+    dim3 grid((1 << log2N) / 1024, M, 1);
+    for(; log2N > ELEMENTARY_LOG2SIZE; log2N -= 2, N >>= 2, M <<= 2){
+        fwtBatch2Kernel<<<grid, 256>>>(d_Data, d_Data, N / 4);
+        CHECK_CUDA_ERROR(cudaPeekAtLastError());
+    }
+
+    fwtBatch1Kernel<<<M, N / 4, N * sizeof(float)>>>(d_Data, d_Data, log2N);
+    CHECK_CUDA_ERROR(cudaPeekAtLastError());
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Modulate two arrays
 ////////////////////////////////////////////////////////////////////////////////
@@ -168,9 +263,41 @@ __global__ void modulateKernel(double *d_A, double *d_B, int N){
         d_A[pos] *= d_B[pos] * rcpN;
 }
 
+__global__ void modulateKernel(float *d_A, float *d_B, int N){
+    int        tid = blockIdx.x * blockDim.x + threadIdx.x;
+    int numThreads = blockDim.x * gridDim.x;
+    float     rcpN = 1.0f / (float)N;
+
+    for(int pos = tid; pos < N; pos += numThreads)
+        d_A[pos] *= d_B[pos] * rcpN;
+}
+
 //Interface to modulateKernel()
 void modulateGPU(double *d_A, double *d_B, int N) {
     modulateKernel<<<128, 256>>>(d_A, d_B, N);
+}
+
+void modulateGPU(float *d_A, float *d_B, int N) {
+    modulateKernel<<<128, 256>>>(d_A, d_B, N);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Calculate output relative error
+////////////////////////////////////////////////////////////////////////////////
+__forceinline__  __device__ float relative_error(double rhs, float lhs) {
+	return __fdividef(lhs, float(rhs));
+}
+
+__global__ void relative_error_kernel(double *output, float *output_rp, float *err_output, int N) {
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (tid < N)
+        err_output[tid] = relative_error(output[tid], output_rp[tid]);
+}
+
+void relative_error_gpu(double *output, float *output_rp, float *err_output, int N) {
+    int blockSize = 32;
+    int gridDim = (N + blockSize - 1) / blockSize;
+    relative_error_kernel<<<gridDim, blockSize>>>(output, output_rp, err_output, N);
 }
 
 #endif
